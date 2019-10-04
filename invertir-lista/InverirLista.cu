65b6770d5558b0fbf5d2aa7df87ee3dc1494dbad
LuisMartinGarciaSebastian <luis.garcia9408@gmail.com>
2019-10-04 17:03:35 -0500

    Invertir lista en Paralelo

--- invertir-lista/InverirLista.cu
+++ invertir-lista/InverirLista.cu
@@ -1,26 +1,42 @@
 #include <stdio.h>
-#define N 4
-#define TPB 32
+#define TPB 1024
 
-__global__ void invertir(int *salida, int *lista ){
-
+__global__ void invertirLista(int *in, int *out){
+    const int idxIn = threadIdx.x;
+    const int idxOut = blockDim.x - 1 - idxIn;
+    out[idxOut] =  in[idxIn];
 }
 
-int main()
-{
-    printf( "Entre un lista de 4 para invertir: ");
-    int *lista[4];
-    int *salida;
+int main(){
+    unsigned int size =  TPB * sizeof(int);
+    
+    int* h_in = (int*) malloc(size);
+    
     int i;
-    for( i = 0 ; i < 4; i++){
-        scanf("%d", &lista[i]);
+    for( i = 0; i < TPB; i++){
+        h_in[i] =  i;
     }
-    cudaMallaloc(&salida, N*sizeof(int));
-    invertir<<<N/TPB, TPB>>>(salida, lista);
-    printf("La lista es: ");
 
-    for (i = 0; i < 4; i++){
-        printf("%d", lista[i]);
+    int *d_in; cudaMalloc((void**)&d_in, size);
+    int *d_out; cudaMalloc((void**)&d_out, size);
+
+    cudaMemcpy(d_in, h_in, size, cudaMemcpyHostToDevice);
+    
+    invertirLista<<<1, TPB>>>(d_in, d_out);
+    
+    int* h_out = (int*) malloc(size);
+
+    cudaMemcpy(h_out, d_out, size, cudaMemcpyDeviceToHost);
+    
+    cudaFree(d_in); cudaFree(d_out);
+
+    printf(" IN / OUT \n");
+
+    for(i = 0; i < TPB; i++){
+        printf(" %d / %d \n", h_in[i], h_out[i]);
     }
+    
+    free(h_in); free(h_out);
+    
+    return 0;
 }
-
