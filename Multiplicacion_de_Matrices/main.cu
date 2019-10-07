6d3d8b9fac76cab3d73fb7f959b1092d36adc83d
LuisMartinGarciaSebastian <luis.garcia9408@gmail.com>
2019-10-07 15:34:23 -0500

    Se agregan comentarios.

--- Multiplicacion_de_Matrices/main.cu
+++ Multiplicacion_de_Matrices/main.cu
@@ -7,12 +7,12 @@ using namespace std;
 __global__ void MultiplicarMatricesSecuencial(float *matriz1_GPU, float *matriz2_GPU, float *matriz3_GPU, int TDM, size_t pitch){ //Este modulo usa 1 solo thread
     for(int i =0; i < TDM; i++){
         for(int j = 0; j < TDM; j++){
-            float *elementos_matriz1 = (float *) ((char*)matriz1_GPU + j * pitch);
+            float *elementos_matriz1 = (float *) ((char*)matriz1_GPU + j * pitch);                      //Obtenemos el j-esimo renglon de la matriz
             float *elementos_matriz3 = (float *) ((char*)matriz3_GPU + j * pitch);
             elementos_matriz3[i] = 0;
             for(int x = 0; x < TDM; x++){
-                float *elementos_matriz2 = (float *) ((char*)matriz2_GPU + (x) * pitch); 
-                elementos_matriz3[i] += elementos_matriz1[x] * elementos_matriz2[i];
+                float *elementos_matriz2 = (float *) ((char*)matriz2_GPU + (x) * pitch);                
+                elementos_matriz3[i] += elementos_matriz1[x] * elementos_matriz2[i];                    //Sumamos en la i-esima laumna del renglon de la matriz que trabajamos
                 free(elementos_matriz2);
                 
             }
@@ -42,17 +42,17 @@ int main(){
     int TDM = 50;
     
     int TDM2 = 1;
-    unsigned int NDH = pow(TDM2,2); //numero de hilos#define TDM  820// Tamño de la matriz (cuadrada)
-    unsigned int numero_bloques =  ceil( (float) NDH / (float) TDM2);// Tamño de la matriz (cuadrada)TDM ); 
-    unsigned int hilos_bloque = ceil( (float) NDH / (float) numero_bloques);// Tamño de la matriz (cuadrada)ero_bloques );
+    unsigned int NDH = pow(TDM2,2);                                             // Número de hilos  
+    unsigned int numero_bloques =  ceil( (float) NDH / (float) TDM2);           // Tamño de la matriz (cuadrada)TDM ); 
+    unsigned int hilos_bloque = ceil( (float) NDH / (float) numero_bloques);    // Tamño de la matriz (cuadrada)ero_bloques );
 
-    float matriz1_host[TDM][TDM];
+    float matriz1_host[TDM][TDM];                                               //Creamos arrglos bidimensionales en la CPU
     float matriz2_host[TDM][TDM];
 
     for(int i = 0; i < TDM; i++){
         for(int j = 0; j < TDM; j++){
-            matriz1_host[i][j] = (int)(i + j);//(float)(rand() % 10);
-            matriz2_host[i][j] = (int)(i + j);//(float)(rand() % 10);
+            matriz1_host[i][j] = (int)(i + j);                                  //Llenamos la matriz que creamos antes con valores
+            matriz2_host[i][j] = (int)(i + j);
         }   
     }
     /* ********** Muestra las matrices que se van a multiplicar
@@ -71,31 +71,31 @@ int main(){
         cout << "\n";
     }
     */
-    size_t pitch;
-
-    float *matriz1_GPU; cudaMallocPitch(&matriz1_GPU, &pitch, TDM * sizeof(float), TDM );
-    float *matriz2_GPU; cudaMallocPitch(&matriz2_GPU, &pitch, TDM * sizeof(float), TDM );
+    size_t pitch;                                                                                       //  Esta variable contiene el tamaño de los espacios 
+                                                                                                        //interiores de la matriz
+    float *matriz1_GPU; cudaMallocPitch(&matriz1_GPU, &pitch, TDM * sizeof(float), TDM );               //  Hacemos las reservaciones en memoria de las 
+    float *matriz2_GPU; cudaMallocPitch(&matriz2_GPU, &pitch, TDM * sizeof(float), TDM );               //matrices que vamos a necesitar
     float *matriz3_GPU; cudaMallocPitch(&matriz3_GPU, &pitch, TDM * sizeof(float), TDM );
 
-    cudaMemcpy2D(matriz1_GPU, pitch, matriz1_host, TDM * sizeof(float), TDM * sizeof(float), TDM, cudaMemcpyHostToDevice);
-    cudaMemcpy2D(matriz2_GPU, pitch, matriz2_host, TDM * sizeof(float), TDM * sizeof(float), TDM, cudaMemcpyHostToDevice);
+    cudaMemcpy2D(matriz1_GPU, pitch, matriz1_host, TDM * sizeof(float), TDM * sizeof(float), TDM, cudaMemcpyHostToDevice); //Copiamos los valores de la matriz en
+    cudaMemcpy2D(matriz2_GPU, pitch, matriz2_host, TDM * sizeof(float), TDM * sizeof(float), TDM, cudaMemcpyHostToDevice); //en la CPU a la GPU
 
-    cudaEvent_t inicio, alto; //Variables para el control de los eventos
-    float tiempo_computo; // Varibale para almacenar el tiempo trancurrido (ms)
+    cudaEvent_t inicio, alto;                                                                               //Variables para el control de los eventos
+    float tiempo_computo;                                                                                   // Varibale para almacenar el tiempo trancurrido (ms)
 
     for(TDM2 = 1; TDM2 <= TDM; TDM2++){
         
-        NDH = pow(TDM2,2); //numero de hilos#define TDM  820// Tamño de la matriz (cuadrada)
-        numero_bloques =  ceil( (float) NDH / (float) TDM2);// Tamño de la matriz (cuadrada)TDM ); 
-        hilos_bloque = ceil( (float) NDH / (float) numero_bloques);// Tamño de la matriz (cuadrada)ero_bloques );
-        tiempo_computo = 0; 
-        cudaEventCreate(&inicio); cudaEventCreate(&alto); //Creamos los eventos
-        cudaEventRecord(inicio); //Creamos una marca temporal, una especia de bandera 
+        NDH = pow(TDM2,2);                                                                                  //numero de hilos que se lanzarán
+        numero_bloques =  ceil( (float) NDH / (float) TDM2);
+        hilos_bloque = ceil( (float) NDH / (float) numero_bloques);
+        tiempo_computo = 0;                                                                                 //Esta variable contendra el tiempo en ms que demora el evento
+        cudaEventCreate(&inicio); cudaEventCreate(&alto);                                                   //Creamos los eventos
+        cudaEventRecord(inicio);                                                                            //Creamos una marca temporal, una especia de bandera 
         MultiplicarMatricesOn<<<numero_bloques, hilos_bloque>>>(matriz1_GPU, matriz2_GPU, matriz3_GPU, TDM2,  pitch);
-        cudaEventRecord(alto); // Creamos una marca temporal, otra bandera
-        cudaEventSynchronize(alto); // Bloquea la CPU para evitar que se continue con el programa hasta que se completen los eventos
-        cudaEventElapsedTime(&tiempo_computo, inicio, alto); //Calcula el tiempo entre los eventos
-        cudaEventDestroy(inicio); cudaEventDestroy(alto); // Se liberan los espacios  de los eventos para poder medir de nuevo más tarde
+        cudaEventRecord(alto);                                                                              // Creamos una marca temporal, otra bandera
+        cudaEventSynchronize(alto);                                                                         // Bloquea la CPU para evitar que se continue con el programa hasta que se completen los eventos
+        cudaEventElapsedTime(&tiempo_computo, inicio, alto);                                                //Calcula el tiempo entre los eventos
+        cudaEventDestroy(inicio); cudaEventDestroy(alto);                                                   // Se liberan los espacios  de los eventos para poder medir de nuevo más tarde
 
         cout << "Tiempo de computo en n^2 threads para una matriz de "<< TDM2 << ": "<<tiempo_computo << "ms"<<endl;
 
@@ -111,7 +111,7 @@ int main(){
     }
     /* ******************** Muestra la matriz de salda de CUDA
     float matriz_salida[TDM][TDM];
-    cudaMemcpy2D(matriz_salida, TDM * sizeof(float), matriz3_GPU, pitch, TDM * sizeof(float), TDM, cudaMemcpyDeviceToHost);
+    cudaMemcpy2D(matriz_salida, TDM * sizeof(float), matriz3_GPU, pitch, TDM * sizeof(float), TDM, cudaMemcpyDeviceToHost); //Copiamos los datos de la matriz de la GPU a la CPU
     
     cout.precision(3);
 
